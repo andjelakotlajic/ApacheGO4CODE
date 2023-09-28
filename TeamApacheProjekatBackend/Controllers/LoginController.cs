b7b081dec2797acc0d4d9559028cb82a9ca08016
krrllee <krleposkok12@gmail.com>
2023-09-28 10:41:45 +0200

    folderi

---
+++ TeamApacheProjekatBackend/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamApacheProjekatBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
